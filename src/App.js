a72959942de8290cc9c89ce2423b1e5a448af84c
ovchynnikov <vapore18@gmail.com>
2020-08-17 15:52:47 +0300

    React router with NavLink and <Router /> files

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, {Component} from 'react'
+import './App.scss'
+import {Route, NavLink} from 'react-router-dom'
+import About from './About/About'
+import Cars from './Cars/Cars'
+
+class App extends Component {
+  render() {
+
+    return (
+      <div>
+        <nav className="nav">
+          <ul>
+            <li>
+              <NavLink to="/" exact>Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/about" exact
+                activeStyle={{
+                  color: 'blue'
+                }}
+              >About</NavLink>
+            </li>
+            <li>
+              <NavLink to={{
+                pathname: '/cars',
+                search: '?a=1&b=2',
+                hash: 'wfm-hash'
+              }} exact>Cars</NavLink>
+            </li>
+          </ul>
+        </nav>
+
+        <hr/>
+
+        {/*localhost:3000*/}
+        <Route path="/" exact render={() => <h1>Home Page</h1>} />
+        <Route path="/about" component={About} />
+        <Route path="/cars" component={Cars} />
+
+      </div>
+    );
+  }
+}
+
+export default App
